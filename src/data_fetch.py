9f172ec77dd3fc59e10766646f9fdeaa75eb36ea
VISHI MALIK <150807099+VishiMalik@users.noreply.github.com>
2026-01-04 23:12:53 +0530

    Update data_fetch.py

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -1,14 +1,13 @@
 import yfinance as yf
 import pandas as pd
 
-def fetch_history(ticker: str, period = "3y", interval = "1d"):
-  t = yf.Ticker(ticker)
-  df = t.history(period = period, interval = interval)
-  df = df[['Open', 'High', 'Low', 'Close', 'Volume']]
-  df.dropna(inplace = True)
-  return df
+def fetch_history(ticker: str, period="3y", interval="1d"):
+    t = yf.Ticker(ticker)
+    df = t.history(period=period, interval=interval)
+    df = df[['Open','High','Low','Close','Volume']]
+    df.dropna(inplace=True)
+    return df
 
-if __name__ = "__main__":
-   df = fetch_history("AAPL", period = "5y")
-   print(df.tail())
-
+if __name__ == "__main__":
+    df = fetch_history("AAPL", period="5y")
+    print(df.tail())
